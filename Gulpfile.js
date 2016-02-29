1f64f117f914561e9885435b502f7bd45db19b05
Edward Cobbold <edward.cobbold@gmail.com>
2016-02-29 11:07:59 +0000

    Initial generate

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,7 +6,7 @@
 // Set patch name - will be used to name .zip files.
 // Alternatively, when calling 'gulp makeZips' on the command line, pass in the
 // argument '--patchname MyPatchName' to override
-var patchName = 'NoPatchName';
+var patchName = 'Tubes';
 // Initial size of source images - set this to the starting size of the patch
 // E.g. if resizing a 128x patch, set to 128
 var initialSize = 512;
@@ -18,7 +18,7 @@ var ignoreTheseFiles = [
     // Design files
     '**/*.{ai,psb,psd}',
     // Win/OSX system files
-    '**/*.{DS_Store,db}',
+    '**/*.{DS_Store,db}'
 ];
 // Resize these files:
 var resizeables = [
